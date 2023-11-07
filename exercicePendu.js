0ad380298006c99fded43142977e6879c427f880
Martin Delemarre <martin@hbjo-online.com>
2023-11-07 09:12:55 +0100

    Update exercicePendu.js

--- exercicePendu.js
+++ exercicePendu.js
@@ -5,23 +5,33 @@ function Pendu(tofind){
 }
 
 Pendu.prototype.TestChar = function(totest){
+    this.tofind.forEach((char, i) => {
+       if(char === totest.toUpperCase()) this.masque[i] = char;
+    });
+    this.attempts--;
 
+    if(attempts === 0) console.log('Perdu ! Vous navez plus d\'essais');
+    console.log(this.masque.join(' '), `Essais restants : ${this.attempts}`);
 }
-Pendu.prototype.TestWin = function(toWin){
 
+Pendu.prototype.TestWin = function(toWin){
+    if(toWin.toUpperCase() === this.tofind.join('')) return console.log(`Bien ouej, le mot était bien ${toWin.toUpperCase()}`);
+    else {
+        this.attempts--;
+        console.log(`Raté, essai encore`, `Essais restants : ${this.attempts}`);
+        console.log(this.masque.join(' '));
+    }
 }
 Pendu.prototype.GenererMasque = function(){
     if(!this.tofind) console.log('Passez un mot a trouver avant !');
     else{
         this.masque = this.tofind;
         console.log(this.masque);
-        this.masque.forEach(function(char, i){
-            console.log(this.masque);
-            if(i != 0 && i != (this.masque.length - 1)) {
+        this.masque.forEach((char, i) => {
+            if (i != 0 && i != (this.masque.length - 1)) {
                 this.masque[i] = '_'
             }
         });
-
         console.log(this.masque.join(' '));
     }
 }
@@ -29,3 +39,6 @@ Pendu.prototype.GenererMasque = function(){
 
 let pendu = new Pendu('maison');
 pendu.GenererMasque();
+
+pendu.TestChar("a");
+pendu.TestWin('madzdn');
