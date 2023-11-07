414bfc34bad1b0e9e9ade21308a7de9442d10ada
Martin Delemarre <martin@hbjo-online.com>
2023-11-07 16:28:09 +0100

    Update worker.js

--- exercice07-webworkers/worker.js
+++ exercice07-webworkers/worker.js
@@ -1,1 +1,8 @@
-
+self.onmessage = function(event) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${event.data}`)
+    .then(response => response.json())
+    .then(data => {
+        self.postMessage(data);
+    })
+    .catch(e => self.postMessage(e));
+}
